be59eabb664d34b0db3e78f1601647aa5ae441fb
Mark Schmele <mark.schmele@gannett.com>
2015-10-22 21:37:52 -0400

    switch to netbeans structure

--- Sashimi/main.cpp
+++ Sashimi/main.cpp
@@ -1,10 +1,19 @@
-#include <iostream>
+/* 
+ * File:   main.cpp
+ * Author: markschmele
+ *
+ * Created on October 22, 2015, 9:37 PM
+ */
+
+#include <cstdlib>
 
 using namespace std;
 
-int main()
-{
-  cout << "Hello World!" << endl;
-  return 0;
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
 }
 
